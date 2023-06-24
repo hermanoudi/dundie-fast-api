251b7657abb3743d6ef622a859958746a5ef4bef
Hermano Flávio de Moura <hermanoudi@gmail.com>
2023-06-24 10:57:31 -0300

    adicionando paginacao, filtros e ordernacao na api

--- dundie/routes/transaction.py
+++ dundie/routes/transaction.py
@@ -1,9 +1,19 @@
-from fastapi import APIRouter, Body, HTTPException
+from typing import Optional
+from fastapi import APIRouter, Body, HTTPException, Depends
 from dundie.auth import AuthenticatedUser
 from dundie.db import ActiveSession
 from dundie.models import User
+from dundie.models import Transaction
+from dundie.models.serializers import TransactionResponse
+from dundie.models.transaction import Transaction
 from dundie.tasks.transaction import add_transaction, TransactionError
 from sqlmodel import select, Session
+from fastapi_pagination import Page, Params
+from fastapi_pagination.ext.sqlmodel import paginate
+
+from sqlmodel import text
+from sqlalchemy.orm import aliased
+
 
 router = APIRouter()
 
@@ -27,3 +37,45 @@ async def create_transaction(
 
     # At this point there was no error, so we can return
     return {"message": "Transaction added"}
+
+
+# No final do arquivo
+@router.get("/", response_model=Page[TransactionResponse])
+async def list_transactions(
+    *,
+    current_user: User = AuthenticatedUser,
+    session: Session = ActiveSession,
+    params: Params = Depends(),
+    from_user: Optional[str] = None,
+    user: Optional[str] = None,
+    order_by: Optional[str] = None, #&order_by=date, value -date, -value
+):
+    query = select(Transaction)
+
+    # Optional `AND` filters
+    if user:
+        query = query.join(
+            User, Transaction.user_id == User.id
+        ).where(User.username == user)
+    if from_user:
+        FromUser = aliased(User)  # aliased needed to desambiguous the join
+        query = query.join(
+            FromUser, Transaction.from_id == FromUser.id
+        ).where(FromUser.username == from_user)
+
+    # Mandatory access filter
+    # regular users can only see their own transactions
+    if not current_user.superuser:
+        query = query.where(
+            Transaction.user_id == current_user.id | Transaction.from_id == current_user.id # pyright: ignore
+        )
+
+    # Ordering based on &order_by=date (asc) or -date (desc)
+    if order_by:
+        order_text = text(
+          order_by.replace("-", "") + " " + ("desc" if "-" in order_by else "asc")
+        )
+        query = query.order_by(order_text)
+
+    # wrap response_model in a pagination object {"items": [], total, page, size }
+    return paginate(query=query, session=session, params=params)
